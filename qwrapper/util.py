b608707c72a1519bae7f1c610db919618e211139
Kohei Nakaji <koheinakaji@KoheinoMacBook-Pro.local>
2022-04-27 07:48:05 +0900

    add excludes to parity

--- qwrapper/util.py
+++ qwrapper/util.py
@@ -3,18 +3,24 @@ from qwrapper.encoder import Encoder
 
 class QUtil:
     @classmethod
-    def parity(cls, sample):
+    def parity(cls, sample, excludes: set = None):
+        if excludes is None:
+            excludes = {}
         r = 1
-        for s in sample:
+        for j, s in enumerate(sample):
+            if j in excludes:
+                continue
             if s == 1:
                 r = r * -1
         return r
 
     @classmethod
-    def exp_parity(cls, state_vector, nqubit):
+    def exp_parity(cls, state_vector, nqubit, excludes: set = None):
+        if excludes is None:
+            excludes = {}
         encoder = Encoder(nqubit)
         result = 0
         for i, s in enumerate(state_vector):
-            parity = cls.parity(encoder.encode(i))
+            parity = cls.parity(encoder.encode(i), excludes)
             result = result + parity * pow(abs(s), 2)
         return result
