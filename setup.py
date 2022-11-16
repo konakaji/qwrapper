e331d130d8117a6a071a242fcfe77292dea81ed3
knakaji <nakajijiji@gmail.com>
2022-11-15 21:34:34 -0500

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qwrapper",
-    version="0.2.5",
+    version="0.2.6",
     author="kouhei nakaji",
     author_email="nakajijiji@gmail.com",
     description="You can receive the message 'Hello!!!'",
