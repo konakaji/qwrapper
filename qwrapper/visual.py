49d5803ebe3e94cf2ca60529997a7ba800489951
knakaji <nakajijiji@gmail.com>
2022-11-15 21:49:09 -0500

    fix

--- qwrapper/visual.py
+++ qwrapper/visual.py
@@ -4,18 +4,27 @@ from qutip import Bloch3d, basis
 
 
 def generate_random_bloch():
-    b = Bloch3d()
-    b.xlabel = ['', '']
-    b.ylabel = ['', '']
-    b.vector_color = ['#000000', 'b', 'y']
-    b.sphere_color = '#008000'
-    b.sphere_alpha = 0.5
-    b.font_scale = 0.2
+    b = initialize()
     x1 = random.uniform(-1.0, 1.0)
     x2 = random.uniform(-math.sqrt(1 - x1 ** 2), math.sqrt(1 - x1 ** 2))
     x3 = random.choice([-math.sqrt(1 - x1 ** 2 - x2 ** 2), math.sqrt(1 - x1 ** 2 - x2 ** 2)])
     b.add_vectors([x1, x2, x3])
     return b
 
-if __name__ == '__main__':
-    generate_random_bloch().show()
+
+def initialize():
+    b = Bloch3d()
+    b.xlabel = ['', '']
+    b.ylabel = ['', '']
+    b.vector_color = ['#000000', 'b', 'y']
+    b.sphere_color = '#008000'
+    b.sphere_alpha = 0.2
+    b.vector_width = 6
+    b.font_scale = 0.2
+    return b
+
+
+def generate_base_bloch():
+    b = initialize()
+    b.add_vectors([0, 0, 1.0])
+    return b
