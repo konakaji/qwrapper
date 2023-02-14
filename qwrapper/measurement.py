9dc144df55a35c5c44633d2012ea7f48e6688054
knakaji <nakajijiji@gmail.com>
2023-02-14 09:41:06 -0500

    add measurement method

---
+++ qwrapper/measurement.py
@@ -0,0 +1,21 @@
+from qwrapper.obs import Hamiltonian
+
+
+class MeasurementMethod:
+    def __init__(self, hamiltonian: Hamiltonian):
+        self.hamiltonian = hamiltonian
+
+    def get_value(self, ntotal, prepare):
+        res = 0
+        nshot = ntotal / len(self.hamiltonian.paulis)
+        for h, p in zip(self.hamiltonian.hs, self.hamiltonian.paulis):
+            qc = prepare()
+            res += h * p.get_value(qc, nshot)
+        return res
+
+    def exact_value(self, prepare):
+        res = 0
+        for h, p in zip(self.hamiltonian.hs, self.hamiltonian.paulis):
+            qc = prepare()
+            res += h * p.exact_value(qc)
+        return res
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qwrapper",
-    version="0.3.2",
+    version="0.3.3",
     author="kouhei nakaji",
     author_email="nakajijiji@gmail.com",
     description="You can receive the message 'Hello!!!'",
