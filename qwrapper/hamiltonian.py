77f054d3fb7d9f869f845eedcc44f9d148b9dad5
knakaji <nakajijiji@gmail.com>
2023-02-14 22:22:10 -0500

    add Heisenberg Hamiltonian

---
+++ qwrapper/hamiltonian.py
@@ -0,0 +1,51 @@
+from qwrapper.obs import Hamiltonian
+from qwrapper.operator import ControllablePauli
+
+
+class HeisenbergModel(Hamiltonian):
+    def __init__(self, nqubit, magnetic=0):
+        hs = []
+        paulis = []
+        coeffs1, p1 = self.interactions(nqubit)
+        hs.extend(coeffs1)
+        paulis.extend(p1)
+        if magnetic != 0:
+            p2 = self.magnetic(nqubit)
+            paulis.extend(p2)
+            hs.extend([magnetic for _ in range(nqubit)])
+        super().__init__(hs, paulis, nqubit)
+
+    def interactions(self, nqubit):
+        coeffs = []
+        results = []
+        for j in range(nqubit):
+            p1 = j
+            p2 = (j + 1) % nqubit
+            for char in ["X", "Y", "Z"]:
+                coeffs.append(1)
+                results.append(self.two_pauli(p1, p2, char, nqubit))
+        return coeffs, results
+
+    def magnetic(self, nqubit):
+        results = []
+        for j in range(nqubit):
+            for char in ["X"]:
+                results.append(self.one_pauli(j, char, nqubit))
+        return results
+
+    def one_pauli(self, p1, char, nqubit):
+        return self._pauli([p1], char, nqubit)
+
+    def two_pauli(self, p1, p2, char, nqubit):
+        return self._pauli([p1, p2], char, nqubit)
+
+    def _pauli(self, ps, char, nqubit):
+        ps = set(ps)
+        results = []
+        for j in range(nqubit):
+            if j in ps:
+                results.append(char)
+            else:
+                results.append("I")
+        p_string = "".join(results)
+        return ControllablePauli(p_string)
