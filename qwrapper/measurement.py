51870c61c12c71aa75faada3a943187d6855001d
knakaji <nakajijiji@gmail.com>
2023-04-04 16:04:46 -0400

    add multiple return for measurementMethod

--- qwrapper/measurement.py
+++ qwrapper/measurement.py
@@ -1,4 +1,5 @@
 from qwrapper.obs import Hamiltonian
+import numpy as np
 import random
 
 
@@ -7,20 +8,26 @@ class MeasurementMethod:
         self.hamiltonian = hamiltonian
 
     def get_value(self, prepare, ntotal=0, seed=None):
+        return np.sum(self.get_values(prepare, ntotal, seed))
+
+    def get_values(self, prepare, ntotal=0, seed=None):
         if seed is not None:
             random.seed(seed)
         if ntotal == 0:
             return self.exact_value(prepare)
-        res = 0
+        res = []
         nshot = ntotal / len(self.hamiltonian.paulis)
         for h, p in zip(self.hamiltonian.hs, self.hamiltonian.paulis):
             qc = prepare()
-            res += h * p.get_value(qc, nshot)
+            res.append(h * p.get_value(qc, nshot))
         return res
 
     def exact_value(self, prepare):
-        res = 0
+        return np.sum(self.exact_values(prepare))
+
+    def exact_values(self, prepare):
+        res = []
         for h, p in zip(self.hamiltonian.hs, self.hamiltonian.paulis):
             qc = prepare()
-            res += h * p.exact_value(qc)
+            res.append(h * p.exact_value(qc))
         return res
